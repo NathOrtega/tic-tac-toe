e7472b175bad4259fe738c7e7912b83c18c68ca8
Nath Ortega <nath_0563@hotmail.com>
2021-07-31 09:34:24 -0500

    Applying requested changes to simplify the code

--- script.js
+++ script.js
@@ -5,16 +5,14 @@ const titleToChange=document.getElementById("title2");
 const mobileDecorationImage=document.getElementById("mobileImage");
 const restartButton=document.getElementById("restartButton");
 const board=[["", "", ""],["", "", ""],["", "", ""]];
-// todo: make const and move inside play
-let clickedButton="";
-// probably, make parameter
-let someoneWins=false;
-// probably, make parameter
-let isTheBoardFull=false;
 
 for (const button of buttons){
   button.addEventListener("click", play);
 }
+
+restartButton.addEventListener("click", () => {
+  window.location.reload();
+});
 
 function updateBoard() {
   board[0][0]=buttons[0].value;
@@ -30,93 +28,71 @@ function updateBoard() {
   board[2][2]=buttons[8].value;
 }
 
-/**
- * TODO: Rename to getWinner,
- * this method should return the winner or undefined
- */
-function checkIfSomeoneWins() {
+function getWinner() {
   const scorpion="SSS", mileena="MMM";
-  let mainDiagonal=""; 
   let secondaryDiagonal=board[2][0] + board[1][1] + board[0][2];
   for (let i=0; i<board.length; i++){
-    let row="", column="";
+    let row="", column="", mainDiagonal="";
     for (let j=0; j<board.length; j++){
       row+=board[i][j];
       column+=board[j][i];
-      // todo: reset this value
       mainDiagonal+=board[j][j];
-      // todo: replace to use [].includes
-      if (row===scorpion || row===mileena){
-        someoneWins=true;
-        break;
-      } else if (column===scorpion || column===mileena){
-        someoneWins=true;
-        break;
-      } else if (mainDiagonal===scorpion || mainDiagonal===mileena){
-        someoneWins=true;
-        break;
-      } else if (secondaryDiagonal===scorpion || secondaryDiagonal===mileena){
-        someoneWins=true;
-        break;
+      const boardArray=[row, column, mainDiagonal, secondaryDiagonal];
+      if (boardArray.includes(scorpion)){
+        return "S";
+      } else if (boardArray.includes(mileena)){
+        return "M";
       }
     }
   }
 }
 
-/**
- * TODO: Split into:
- * showWinnerResult(winner S|M)
- * showTiedResult()
- */
-function selectWinner(){
-  if (someoneWins){
-    titleToHide.style.display="none";
-    playerEmoji.src=playerEmoji.src.includes("Scorpion") ? `./Resources/Scorpion.png` : `./Resources/Mileena.png`;
-    titleToChange.innerText="wins!"
-  } else if (!someoneWins && isTheBoardFull) {
-    titleToHide.style.display="none";
-    playerEmoji.style.display="none";
-    titleToChange.innerText="You're both losers!";
-    titleToChange.style.textAlign="center";
+function showWinnerResult(winner){
+  titleToHide.style.display="none";
+  playerEmoji.src=winner==="S" ? `./Resources/Scorpion.png` : `./Resources/Mileena.png`;
+  titleToChange.innerText="wins!"
+}
+
+function showTiedResult(){
+  titleToHide.style.display="none";
+  playerEmoji.style.display="none";
+  titleToChange.innerText="You're both losers!";
+  titleToChange.style.textAlign="center";
+}
+
+function disableBoardButtons(){
+  for (const button of buttons){
+    button.removeEventListener("click", play);
   }
 }
 
+function showRestartButton() {
+  restartButton.style.display="inline";
+  mobileDecorationImage.style.marginTop="0";
+}
+
 function play(event) {
-  clickedButton=document.getElementById(event.target.id);
+  const clickedButton=document.getElementById(event.target.id);
   clickedButton.value=playerEmoji.src.includes("Scorpion") ? "S" : "M";
   clickedButton.style.backgroundImage=`url(${playerEmoji.src})`;
   clickedButton.style.backgroundSize="cover";
   clickedButton.style.backgroundColor="#860701";
   clickedButton.removeEventListener("click", play);
-
+  
   updateBoard();
-  // todo: make local const
-  isTheBoardFull=buttons.every(e => e.value!==undefined);
-  // const winner = getWinner() // S|M|undefined
-  checkIfSomeoneWins();
+  
+  const isTheBoardFull=buttons.every(e => e.value!==undefined);
+  const winner=getWinner();
 
-  selectWinner();
-
-  showRestartButtonIfNecessary();
-
-  if(!someoneWins){
+  if (winner!==undefined){
+    showWinnerResult(winner);
+    disableBoardButtons();
+    showRestartButton();
+  } else if (isTheBoardFull){
+    showTiedResult();
+    disableBoardButtons();
+    showRestartButton();
+  } else {
     playerEmoji.src=playerEmoji.src.includes("Scorpion") ? `./Resources/Mileena.png` : `./Resources/Scorpion.png`;
-  }
-}
-
-restartButton.addEventListener("click", () => {
-  window.location.reload();
-});
-
-// TOOD: rename to showRestartButton
-// remove conditional.
-function showRestartButtonIfNecessary() {
-  if (someoneWins || isTheBoardFull){
-    restartButton.style.display="inline";
-    mobileDecorationImage.style.marginTop="0";
-
-    for (const button of buttons){
-      button.removeEventListener("click", play);
-    }
   }
 }
